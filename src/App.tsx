c207443d130c3518140f23f65898c9b0e92e255d
Sergio Gabriel <sergiogscruz@gmail.com>
2022-04-10 17:07:58 -0300

    feat: interface para busca de filmes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import Logo from "./search.svg";
+const API_URL = `http://www.omdbapi.com?apikey=691b69b2`
 
 const App = () => {
+  const searchMovies = async (title: string) : Promise<any> => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log('data',data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <h1>React Movie</h1>
+      <div className='search'>
+        <input 
+          placeholder='Busque por um filme'
+          value="Superman"
+          onChange={() => {}} 
+          />
+        <img src={Logo} alt="" />
+      </div>
+    </div>
   );
 }
 
